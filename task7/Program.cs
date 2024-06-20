775c2cc31294359343f82af4aa127c717e6313f3
Administrator <Administrator@WIN-S09DU0P86JK>
2024-06-20 16:36:14 +0300

    Добавьте файлы проекта.

---
+++ task7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CurrencyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Предположим, что курс обмена 1 USD = 0.85 EUR
+            double exchangeRate = 0.85;
+
+            Console.WriteLine("Введите сумму в USD для конвертации в EUR:");
+            double usdAmount = Convert.ToDouble(Console.ReadLine());
+
+            double eurAmount = usdAmount * exchangeRate;
+
+            Console.WriteLine($"{usdAmount} USD равно {eurAmount} EUR");
+        }
+    }
+}
